17ff1998b7b0338ceff7845fe40e0ddd9b696580
Tobias Kiertscher <dev@mastersign.de>
2019-02-22 11:46:38 +0100

    fixed Bash script for test scenario 1

--- auto/run-test-setup-1.sh
+++ auto/run-test-setup-1.sh
@@ -6,9 +6,9 @@ cd "$this_dir"
 
 rm test-1-*.log
 
-xterm -title "Log Listen" -geometry 160x20 -e bash -c "cd $pdir && pipenv run python3 -m logsweet.cli listen 127.0.0.1:9501 127.0.0.1:9502" &
-xterm -title "Log Broadcast A" -geometry 120x20 -e bash -c "cd $pdir && pipenv run python3 -m logsweet.cli broadcast -s -b 127.0.0.1:9501 \"$this_dir/a-*.log\"" &
-xterm -title "Log Broadcast B" -geometry 120x20 -e bash -c "cd $pdir && pipenv run python3 -m logsweet.cli broadcast -s -b 127.0.0.1:9502 \"$this_dir/b-*.log\"" &
+xterm -title "Log Listen" -geometry 160x20 -e bash -c "cd \"$pdir\" && pipenv run python3 -m logsweet.cli listen 127.0.0.1:9501 127.0.0.1:9502" &
+xterm -title "Log Broadcast A" -geometry 120x20 -e bash -c "cd \"$pdir\" && pipenv run python3 -m logsweet.cli broadcast -s -b 127.0.0.1:9501 \"$this_dir/test-1-a-*.log\"" &
+xterm -title "Log Broadcast B" -geometry 120x20 -e bash -c "cd \"$pdir\" && pipenv run python3 -m logsweet.cli broadcast -s -b 127.0.0.1:9502 \"$this_dir/test-1-b-*.log\"" &
 
 log_files="\"$this_dir/test-1-a-1.log\" \"$this_dir/test-1-a-2.log\" \"$this_dir/test-1-b-1.log\" \"$this_dir/test-1-b-2.log\""
 xterm -title "Log Mock" -geometry 120x20 -e bash -c "cd \"$pdir\" && pipenv run python3 -m logsweet.cli mock -i 0.001 $log_files" &
